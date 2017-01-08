c5fa761565147cf44d75f0d7208a310b8c3079de
juana sanchez <juana sanchez>
2017-01-08 15:07:58 -0800

    Signed-off-by: juana sanchez <juana sanchez>

---
+++ Homework/Assignment_1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: jpela_000
+ *
+ * Created on January 8, 2017, 1:47 PM
+ *///Sum of two Numbers
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int nOne,nTwo,total;
+    
+    nOne=99;
+    nTwo=62;
+    total= nOne+nTwo;
+            
+    cout<<"99 + 62 = "<< total << endl;
+    return 0;
+}
+
